e19597e98218a630dfdf7433b23b66b4f972e821
1619413 <1619413@students.wits.ac.za>
2021-04-14 18:40:42 +0200

    Getting server start to work

--- server/startServer.js
+++ server/startServer.js
@@ -1,6 +1,4 @@
-var server = app.listen(8080, () => {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log(`Listening at http://${host}:${port}`);
-})
+const app = require("./mongoCaller.js");
+app.listen(8080, () => {
+  console.log("Server has started!");
+});
