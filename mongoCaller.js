ebd235cca68a686a9a312fc5c30d1acd6b46efb3
Anushka Monema <anushka.monema@gmail.com>
2021-09-27 22:58:12 +0200

    Testing???

--- mongoCaller.js
+++ mongoCaller.js
@@ -48,7 +48,7 @@ MongoClient.connect(url, { useUnifiedTopology: true })
 
     })
 
-    app.post('/addDegree', (request, response) => {
+    app.put('/addDegree', (request, response) => {
         degreeRequirement.insertOne(
             request.body
         )
